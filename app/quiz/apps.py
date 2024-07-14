8148aa6b483a7d89c120c0f68f799aee10fafcbc
stevenkaluwe <107605167+stevenpsl@users.noreply.github.com>
2024-07-14 08:46:08 +0200

    commit#1

---
+++ app/quiz/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class QuizConfig(AppConfig):
+    default_auto_field='django.db.models.BigAutoField'
+    name = 'quiz'
---
+++ app/quiz/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.start_quiz_view, name='start'),
+  path('get-questions/start/start', views.get_questions, {'is_start': True}, name='get-questions'),
+  path('get-questions/start', views.get_questions, {'is_start': False}, name='get-questions'),
+  path('get-answer', views.get_answer, name='get-answer'),
+  path('get-finish', views.get_finish, name='get-finish'),
+  path('feedback', views.feedback, name='rate'),
+]
+
+
